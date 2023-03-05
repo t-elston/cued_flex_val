5832586c8b7becab29711147edeaa5fa3c753d66
thom <103864421+t-elston@users.noreply.github.com>
2023-03-04 19:26:43 -0800

    Delete MATLAB_recording_analysis directory

--- MATLAB_recording_analysis/aaa_psuedopop_analysis_v01.m
+++
@@ -1,9 +0,0 @@
-%%% aaa_psuedopop_analysis_v01
-
-% first, aggregate the data across the files for the pseudo-pop
-datadir = 'C:\Users\Thomas Elston\Documents\MATLAB\Projects\CuedFlexVal\recording_data\';
-
-n_boots = 100;
-[train_labels, train_firing_rates, test_labels, test_firing_rates, OFC_ix] = pseudo_aggregate_data_v01(n_boots, datadir);
-
-[HPC_accs, OFC_accs] = pseudo_LDA_v01(train_labels, train_firing_rates, test_labels, test_firing_rates, OFC_ix, n_boots);
